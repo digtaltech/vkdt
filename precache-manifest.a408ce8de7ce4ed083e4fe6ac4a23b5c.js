b2e0a8d4ffb97643ebac7146ddbd8a6dce4b38a9
Digital Tech <dt.team.prod@gmail.com>
2019-09-30 18:17:18 +0300

    Updates

---
+++ precache-manifest.a408ce8de7ce4ed083e4fe6ac4a23b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dac907d2d84881044b3b",
+    "url": "/vkdt/static/js/runtime~main.b55bc745.js"
+  },
+  {
+    "revision": "15779c13209b29574d07",
+    "url": "/vkdt/static/js/main.970e79cb.chunk.js"
+  },
+  {
+    "revision": "1f2900fc888dc4bf0d70",
+    "url": "/vkdt/static/js/2.bf67f72e.chunk.js"
+  },
+  {
+    "revision": "15779c13209b29574d07",
+    "url": "/vkdt/static/css/main.890adeba.chunk.css"
+  },
+  {
+    "revision": "1f2900fc888dc4bf0d70",
+    "url": "/vkdt/static/css/2.f44e6981.chunk.css"
+  },
+  {
+    "revision": "5f17d2b605560e7016384187cad32cdc",
+    "url": "/vkdt/index.html"
+  }
+];
